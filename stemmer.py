91b360455704fd13c5b2bb011d78cca912de7648
Piezoid <piezo.w.dimd@gmail.com>
2016-02-01 16:09:41 +0100

    Réecriture du lemmatizer (aka stemmer).

--- stemmer.py
+++ stemmer.py
@@ -1,50 +1,80 @@
+import unicodedata
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+
 
 # Mots qui peuvent être transformé en faux amis par les regles 'suffixes'
-defaultInvariants = 'due one max use six its she may they sex user normal here que Ile Paris Rennes poste ete mais mois tél aller ENS ens celle regarder lorsque pays dos poster Latex EST'
+defaultInvariants = 'I sure due done one issue max use six its she may they sex user normal here que Ile Paris Rennes basé à mis poste ete dès tél ENS base gene Latex EST CET unix linux'
 
 # Mots dont le stem est irrégulier
 defaultMap = {'ses': 'son',
               'nos': 'notre',
               'vos': 'votre',
-              'bonne': 'bon',
               'eaux': 'eau',
-              'were': 'be',
-              'was': 'be',
-              'are': 'be',
-              'seen': 'see',
-              'shown': 'show',
-              'hidden': 'hide',
-              'went': 'go',
-              'gone': 'go',
-              'sent': 'sent',
-              'had': 'have',
-              'has': 'have',
-              'said': 'say',
-              'done': 'do',
-              'an': 'a'
+              'ens': 'ENS',
+              'la': 'le',
+
+
+              'mais': 'but',
+              'dans': 'in',
+              'lorsque': 'when',
+              'pays': 'country',
+              'donné': 'data',
+              'dos': 'back',
+              'équipe': 'team',
+              'mois': 'mounth',
+
             }
 
-suffixes = [
-            ('ing', ''),
-            ('ation', 'er'),
-            ('ique', 'ic'),
-            ('ical', 'y'),
+def lower(w):
+    if len(w) > 1:
+        yield w.lower()
 
-            ('ies', 'y'),
-            ('eaux', 'au'),
-            ('aux' , 'al'),
-            ('aux', 'ail'),
-            ('s' , ''),
-            ('x' , ''),
+def stripSuffixes(suffixes):
+    def go(w):
+        lenw = len(w)
+        if lenw < 2 or w[-1] == w[-2]: # less, see, etc
+            return
+        for suf, repl in suffixes:
+            suflen = len(suf)
+            if w.endswith(suf) and len(w) >= suflen + 2:
+                yield w[:-suflen] + repl
+    return go
 
-            ('elle', 'el'),
-            ('e' , ''),
 
-            ('é' , ''),
-            ('é' , 'er'),
-            ('er', ''),
-            ('ed', ''),
-           ]
+singularize = stripSuffixes([   ('ies', 'y'),
+                                ('eaux', 'au'),
+                                ('aux' , 'al'),
+                                ('aux', 'ail'),
+                                ('s' , ''),
+                                ('x' , '')
+                                ])
+
+masculinize = stripSuffixes([   ('elle', 'eau'),
+                                ('elle', 'el'),
+                                ('nne', 'n'),
+                                ('e' , ''),
+                                ])
+
+def stripHyphen(w):
+    yield w.replace('-', '')
+
+# French -> english
+fr2enSuffixes = stripSuffixes([ ('el', 'al'),
+                                ('ie', 'y'),
+                                ('ique', 'ic'),
+                                ])
+
+def fr2en(w):
+    if len(w) <= 3:
+        return
+    w = strip_accents(w)
+    yield w
+    yield from fr2enSuffixes(w)
+
+
 
 class Stemmer:
     """ Stemmer basé sur un vocabulaire :
@@ -52,55 +82,65 @@ class Stemmer:
     candidats appartenant au vocabulaire.
     Il ne peut inventer de mots, mais il existe toujours la possibilité de faux amis
     """
-    def __init__(self, vocab, invariants=defaultInvariants, initmap=defaultMap, suffixes=suffixes):
+    def __init__(self, vocab, invariants=defaultInvariants, initmap=defaultMap):
         self.vocab = vocab
-        self.suffixes = suffixes
         self.termMap = initmap.copy()
         self.termMap.update({w:w for w in invariants.split()})
+        self.ops = [lower, singularize, masculinize, stripHyphen, fr2en]
 
 
     def stem(self, w):
         termMap = self.termMap
 
-        if w in termMap:
-            return termMap[w]
+        shortcut = termMap.get(w)
+        if shortcut is not None:
+            return shortcut
 
-        lenw = len(w)
+        vocab = self.vocab
+
+        origs = set()
+        origs.add(w)
+
+        for op in self.ops:
+            for candidate in op(w):
+                if candidate != w and candidate in vocab:
+                    origs.add(w)
+                    w = candidate
+                    break
+
+            shortcut = termMap.get(w)
+            if shortcut is not None:
+                w = shortcut
+                break
 
 
-        if lenw <= 2 or w[-1] == w[-2]: # see, fee, less, is, as, etc
-            if w != 'I':
-                return w.lower()
+        for orig in origs:
+            if orig in termMap:
+                assert termMap[orig] != w
             else:
-                return w
-
-
-
-        candidates = set()
-        candidates.add(w.lower())
-
-        for suf, repl in suffixes:
-            suflen = len(suf)
-            if w.endswith(suf) and lenw > suflen:
-                cand = w[:-suflen] + repl
-                candidates.add(cand)
-
-        for candidate in candidates:
-            if candidate == w:
-                continue
-            if candidate in self.vocab and len(candidate) > 1:
-                res = self.stem(candidate)
-                termMap[w] = res
-                return res
-
+                termMap[orig] = w
         return w
 
 
     def printMapping(self):
         termMap = self.termMap
+        reverse = dict()
         for k,v in termMap.items():
-            if not k.islower() and (k.lower() in termMap or k.lower() == v):
-                continue
+            #if not k.islower() and (k.lower() in termMap or k.lower() == v):
+                #continue
             if k == v:
                 continue
-            print(k,'->',v)
+            reverse.setdefault(v, set()).add(k)
+
+        reverse = list(reverse.items())
+        if isinstance(self.vocab, dict):
+            reverse.sort(key = lambda e: sum(self.vocab[k] for k in e[1] if k != e[0]) + self.vocab[e[0]])
+            for v, ks in reverse:
+                ks = ' '.join(k + '~' + str(self.vocab[k]) for k in ks if k.lower() != v)
+                if ks:
+                    v = v + '~' + str(self.vocab[v])
+                    print(ks, '->', v)
+        else:
+            for v, ks in reverse:
+                print(' '.join(ks), '->', v)
+
